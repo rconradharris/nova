4daaf0890d9e1b4adbecdf1e31af6e5593191988
Rick Harris <rconradharris@gmail.com>
2011-12-13 01:16:13 +0000

    More work
    Change-Id: Iaa983febd78e6f2404fad2d2b846f7f6821710bb

--- nova/api/openstack/v2/contrib/services.py
+++ nova/api/openstack/v2/contrib/services.py
@@ -34,30 +34,36 @@ from xml.dom import minidom
 LOG = logging.getLogger("nova.api.contrib.services")
 
 
+SERVICE_DB_ATTRS = ['id', 'host', 'topic', 'disabled', 'report_count',
+                    'updated_at']
+
+COMPUTE_NODE_ATTRS = ['vcpus', 'memory_mb', 'local_gb', 'vcpus_used',
+                      'memory_mb_used', 'local_gb_used', 'hypervisor_type',
+                      'hypervisor_version', 'cpu_info']
+
+
 def _build_service(base_url, service):
-    return {'id': service['id'],
-            'href': os.path.join(base_url, 'services', str(service['id'])),
-            'host': service['host'],
-            'type': service['topic'],
-            'enabled': not service['disabled'],
-            'report_count': service['report_count'],
-            'last_checkin': service['updated_at']}
+    service_info = {}
+    for attr in SERVICE_DB_ATTRS:
+        service_info[attr] = service[attr]
+
+    href = os.path.join(base_url, 'services', str(service['id']))
+    service_info['href'] = href
+    return service_info
 
 
-def _build_compute_service(base_url, service, compute_node, instances):
+def _build_compute_service(req, base_url, service, compute_node, instances):
     compute_info = _build_service(base_url, service)
 
-    # db attributes
-    exposed_attrs = ['vcpus', 'memory_mb', 'local_gb', 'vcpus_used',
-                     'memory_mb_used', 'local_gb_used', 'hypervisor_type',
-                     'hypervisor_version', 'cpu_info']
-    for attr in exposed_attrs:
+    # add in compute node info
+    for attr in COMPUTE_NODE_ATTRS:
         compute_info[attr] = compute_node[attr]
 
     # computed attributes
-    used_memory_mb_servers = sum(inst['memory_mb'] for inst in instances
-                                 if inst['vm_state'] == 'active')
-    compute_info['memory_mb_used_servers'] = used_memory_mb_servers
+    compute_info['memory_mb_used_servers'] = sum(
+        inst['memory_mb'] for inst in instances
+        if inst['vm_state'] == 'active')
+
     return compute_info
 
 
@@ -90,7 +96,7 @@ class ServicesController(object):
                     context, service['id'])
             instances = db.instance_get_all_by_host(context, service['host'])
             details = _build_compute_service(
-                    req.application_url, service, compute_node, instances)
+                req, req.application_url, service, compute_node, instances)
         else:
             details = _build_service(req.application_url, service)
 
@@ -116,13 +122,6 @@ class ServicesController(object):
     def servers(self, req, id):
         context = req.environ['nova.context']
         service = self._safe_service_get(context, id)
-
-        if service['topic'] != 'compute':
-            raise webob.ex.HTTPBadRequest(
-                    'Can only request servers for compute service')
-
-        compute_node = db.compute_node_get_for_service(
-                context, service['id'])
         instances = db.instance_get_all_by_host(context, service['host'])
         return self._view_builder.detail(req, instances)
 
@@ -141,10 +140,8 @@ class Services(extensions.ExtensionDescriptor):
         service = extensions.ResourceExtension('services',
                     ServicesController(),
                     serializer=serializer,
-                    member_actions={'details': 'GET',
-                                    'version': 'GET',
+                    member_actions={'version': 'GET',
                                     'config': 'GET',
-                                    'unitcount': 'GET',
                                     'servers': 'GET'})
 
         return [service]
@@ -156,39 +153,26 @@ nsmap = {None: xmlutil.XMLNS_V11, 'atom': xmlutil.XMLNS_ATOM}
 class ServicesTemplate(xmlutil.TemplateBuilder):
     def construct(self):
         root = xmlutil.TemplateElement('services')
-        root.set('type')
         elem = xmlutil.SubTemplateElement(root, 'service', selector='services')
-        elem.set('id')
+        for attr in SERVICE_DB_ATTRS:
+            elem.set(attr)
         elem.set('href')
-        elem.set('enabled')
-        elem.set('report_count')
-        elem.set('last_checkin')
         return xmlutil.MasterTemplate(root, 1, nsmap=nsmap)
 
 
 class ServiceTemplate(xmlutil.TemplateBuilder):
     def construct(self):
         root = xmlutil.TemplateElement('service', selector='service')
-        root.set('id')
-        elem = xmlutil.SubTemplateElement(root, 'href')
-        elem.text = 'href'
-        elem = xmlutil.SubTemplateElement(root, 'enabled')
-        elem.text = 'enabled'
-        elem = xmlutil.SubTemplateElement(root, 'report_count')
-        elem.text = 'report_count'
-        elem = xmlutil.SubTemplateElement(root, 'last_checkin')
-        elem.text = 'last_checkin'
-        return xmlutil.MasterTemplate(root, 1, nsmap=nsmap)
 
+        def add_elem(attr):
+            elem = xmlutil.SubTemplateElement(root, attr)
+            elem.text = attr
 
-class DetailsTemplate(xmlutil.TemplateBuilder):
-    def construct(self):
-        root = xmlutil.TemplateElement('serviceDetails')
-        elem = xmlutil.SubTemplateElement(root, 'item',
-                                          selector='servicedetails')
-        elem.set('name')
-        elem.set('href')
-        elem.set('description')
+        for attr in SERVICE_DB_ATTRS:
+            add_elem(attr)
+
+        add_elem('href')
+        add_elem('memory_mb_used_servers')
         return xmlutil.MasterTemplate(root, 1, nsmap=nsmap)
 
 
@@ -214,16 +198,6 @@ class ConfigTemplate(xmlutil.TemplateBuilder):
         return xmlutil.MasterTemplate(root, 1, nsmap=nsmap)
 
 
-class UnitCountTemplate(xmlutil.TemplateBuilder):
-    def construct(self):
-        root = xmlutil.TemplateElement('unitCount', selector='unitcount')
-        elem = xmlutil.SubTemplateElement(root, 'memory')
-        elem.text = 'memory'
-        elem = xmlutil.SubTemplateElement(root, 'disk')
-        elem.text = 'disk'
-        return xmlutil.MasterTemplate(root, 1, nsmap=nsmap)
-
-
 class ServiceXMLSerializer(xmlutil.XMLTemplateSerializer):
     def index(self):
         return ServicesTemplate()
@@ -231,17 +205,11 @@ class ServiceXMLSerializer(xmlutil.XMLTemplateSerializer):
     def show(self):
         return ServiceTemplate()
 
-    def details(self):
-        return DetailsTemplate()
-
     def version(self):
         return VersionTemplate()
 
     def config(self):
         return ConfigTemplate()
-
-    def unitcount(self):
-        return UnitCountTemplate()
 
     def servers(self):
         return servers.ServersTemplate()

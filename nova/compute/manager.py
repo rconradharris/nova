30f6e631f1b2de12f6c38d6f40bb45e6c4bca39f
Monsyne Dragon <mdragon@rackspace.com>
2012-05-04 16:30:38 +0000

    Changed launched_at as soon as resize starts... and update rebuild too.

--- nova/compute/manager.py
+++ nova/compute/manager.py
@@ -1071,6 +1071,12 @@ class ComputeManager(manager.SchedulerDependentManager):
         if not instance:
             instance = self.db.instance_get_by_uuid(context, instance_uuid)
 
+        # This is a grievous hack to make usage happy for the moment
+        # We will remove this when their system is fixed. (mdragon)
+        instance = self._instance_update(context,
+                instance['uuid'], launched_at=timeutils.utcnow())
+        # Endhack.
+
         with self._error_out_instance_on_exception(context, instance['uuid']):
             LOG.audit(_("Rebuilding instance"), context=context,
                       instance=instance)
@@ -1496,9 +1502,12 @@ class ComputeManager(manager.SchedulerDependentManager):
 
         """
         migration_ref = self.db.migration_get(context, migration_id)
-        if not instance:
-            instance = self.db.instance_get_by_uuid(context,
-                    migration_ref.instance_uuid)
+        # This is a grievous hack to make usage happy for the moment
+        # We will remove this when their system is fixed. (mdragon)
+        instance = self._instance_update(context,
+                migration_ref.instance_uuid,
+                launched_at=timeutils.utcnow())
+        # Endhack.
 
         # NOTE(comstud): A revert_resize is essentially a resize back to
         # the old size, so we need to send a usage event here.
@@ -1598,6 +1607,12 @@ class ComputeManager(manager.SchedulerDependentManager):
 
         if not instance_type:
             instance_type = instance_types.get_instance_type(instance_type_id)
+
+        # This is a grievous hack to make usage happy for the moment
+        # We will remove this when their system is fixed. (mdragon)
+        instance = self._instance_update(context,
+                instance['uuid'], launched_at=timeutils.utcnow())
+        # Endhack.
 
         with self._error_out_instance_on_exception(context, instance['uuid'],
                                                    reservations):

e58ffca57ce86a179265500617323c29465348c3
Chris Behrens <cbehrens@codestud.com>
2012-08-15 07:04:29 +0000

    For quotas and resize and cells: commit reservations immediately for now
    There's not an immediate good solution for quotas on resize with cells
    due to how it's implemented.  Will have to revisit this with a better
    patch later.  This can lead to some out-of-sync issues when deleting
    things in certain resize states... or if there are resize errors.

--- nova/compute/api.py
+++ nova/compute/api.py
@@ -1454,6 +1454,12 @@ class API(base.Base):
                                task_state=task_states.RESIZE_REVERTING,
                                expected_task_state=None)
 
+        # With cells, the best we can do right now is commit the reservations
+        # immediately...
+        if FLAGS.enable_cells and reservations:
+            QUOTAS.commit(context, reservations)
+            reservations = []
+
         self.compute_rpcapi.revert_resize(context,
                 instance=instance, migration_id=migration_ref['id'],
                 host=migration_ref['dest_compute'], reservations=reservations)
@@ -1480,6 +1486,12 @@ class API(base.Base):
         instance = self.update(context, instance, vm_state=vm_states.ACTIVE,
                                task_state=None,
                                expected_task_state=None)
+
+        # With cells, the best we can do right now is commit the reservations
+        # immediately...
+        if FLAGS.enable_cells and reservations:
+            QUOTAS.commit(context, reservations)
+            reservations = []
 
         self.compute_rpcapi.confirm_resize(context,
                 instance=instance, migration_id=migration_ref['id'],
@@ -1644,6 +1656,12 @@ class API(base.Base):
 
         if not FLAGS.allow_resize_to_same_host:
             filter_properties['ignore_hosts'].append(instance['host'])
+
+        # With cells, the best we can do right now is commit the reservations
+        # immediately...
+        if FLAGS.enable_cells and reservations:
+            QUOTAS.commit(context, reservations)
+            reservations = []
 
         args = {
             "instance": instance,
